40cf9962b93d9577b96a7eec556075769aaa2030
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-27 11:19:11 -0800

    added initial skeleton file for the linked_list project

---
+++ linked_list.rb
@@ -0,0 +1,55 @@
+class LinkedList
+    def initialize
+    end
+
+    def append
+    end
+
+    def prepend
+    end
+
+    def size
+    end
+
+    def head
+    end
+
+    def tail
+    end
+
+    def at(index)
+
+    end
+
+    def pop
+    end
+
+    def contains?
+    end
+
+    def find(data)
+    end
+
+    def to_s
+    end
+
+    def insert_at(index)
+    end
+
+    def remove_at(index)
+    end
+
+    
+
+end
+
+class Node
+    def initialize(value)
+        @value = value
+        @next_node = nil
+    end
+
+    def value
+        @value
+    end
+end
