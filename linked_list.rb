c3fb42ea17a1c605f371e7090182dcca167a1e52
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-27 12:19:11 -0800

    refactored Node class

--- linked_list.rb
+++ linked_list.rb
@@ -1,12 +1,17 @@
+
+###how in the hell do we maintain an Index location of each node?  Can't possibly store them IN the node, but if I store them separately in an array or hash in the LinkedList as a Class variable, doesn't
+###that mean the implementation of the LinkedList is pointless as it's backbone would be a different data structure?!
+
 class LinkedList
     def initialize
         @head = nil
         @tail = nil
         @size = 0
+        @index = 0
     end
 
     def append(value)
-        new_node = Node.new(value)
+        new_node = Node.new(value, @index)
         @size += 1
 
         if @head == nil
@@ -14,11 +19,12 @@ class LinkedList
             @tail = new_node.value
         else
             @tail = new_node.value
+            @index += 1
         end
     end
 
     def prepend
-        new_node = Node.new(value)
+        new_node = Node.new(value, @index)
         @size += 1
 
         if @head == nil
@@ -31,6 +37,7 @@ class LinkedList
     end
 
     def size
+        @size
     end
 
     def head
@@ -68,9 +75,9 @@ class LinkedList
 end
 
 class Node
-    def initialize(value)
+    def initialize(value, next_node = nil)
         @value = value
-        @next_node = nil
+        @next_node = next_node
     end
 
     def value
@@ -87,5 +94,9 @@ class Node
 
     def head
         @head
+    end
+
+    def index
+        @index
     end
 end
